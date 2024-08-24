2898e566bf537c916e94ccb5408d4feb6083a2c3
John Patrick Trinidad <Ptrinidad765@gmail.com>
2024-08-24 22:13:20 +0800

    updated

--- app/new-password/page.tsx
+++ app/new-password/page.tsx
@@ -1,17 +1,20 @@
 import { NextPage } from "next";
 
 import { NewPasswordForm } from "@/forgotpassword/NewPasswordForm";
+import { Suspense } from "react";
 
 type NewPasswordPageProps = {};
 
 const NewPasswordPage: NextPage = async ({}: NewPasswordPageProps) => {
   return (
+    <Suspense>
     <div className="w-full flex flex-col justify-center items-center h-screen bg-gray-100">
       <div className="p-5 rounded border bg-white shadow">
       <NewPasswordForm />
       </div>
      
     </div>
+    </Suspense>
   );
 };
 

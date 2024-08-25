8dd395c4f01af7433e526fe2036f4750dd8d4b09
John Patrick Trinidad <Ptrinidad765@gmail.com>
2024-08-25 17:20:58 +0800

    final touch added

--- components/HomeComponents/HomePage.tsx
+++ components/HomeComponents/HomePage.tsx
@@ -1,11 +1,15 @@
 import { auth } from '@/auth'
 import React from 'react'
+import LogoutButton from '../settingsComponent/LogoutButton'
 
 const HomePage = async () => {
   const session = await auth() 
   return (
-    <div>Home
+    <div className='w-full h-screen flex flex-col items-center justify-center gap-4'>
+      <h1 className='text-3xl font-bold'>Welcome to Authify</h1>
       <p>{session?.user?.name}</p>
+
+      <LogoutButton />
     </div>
   )
 }
